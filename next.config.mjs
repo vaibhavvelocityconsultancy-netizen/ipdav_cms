578b40d494a450aab1276d3393e105af281e82da
Vaibhav Upadhyay <vaibhav.velocityconsultancy@gmail.com>
2026-08-03 16:35:34 +0530

    fix: format next.config.mjs for improved readability

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+
+  basePath: "/newweb",
+  assetPrefix: "/newweb",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   experimental: {
     proxyClientMaxBodySize: "100mb",
   },
