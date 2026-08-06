d87a6b253270649e238becb6373f2aef3db36ba9
Vaibhav Upadhyay <vaibhav.velocityconsultancy@gmail.com>
2026-08-06 11:30:31 +0530

    refactor: remove unused basePath configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === "production";
-const basePath =
-  process.env.NEXT_PUBLIC_BASE_PATH || (isProduction ? "/newweb" : "");
 
 const nextConfig = {
   output: "standalone",
-
-  ...(basePath
-    ? {
-        basePath,
-        assetPrefix: basePath,
-      }
-    : {}),
 
   typescript: {
     ignoreBuildErrors: true,
