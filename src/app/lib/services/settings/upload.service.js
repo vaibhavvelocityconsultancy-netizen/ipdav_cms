b596896959924f9bd0ab73f83f8e40ddf362f33f
Vaibhav Upadhyay <vaibhav.velocityconsultancy@gmail.com>
2026-08-07 12:53:35 +0530

    file upload log added

--- src/app/lib/services/settings/upload.service.js
+++ src/app/lib/services/settings/upload.service.js
@@ -36,9 +36,6 @@ export async function uploadFile(file) {
   const buffer = Buffer.from(bytes);
 
   await fs.writeFile(filePath, buffer);
-  console.log("process.cwd():", process.cwd());
-console.log("Upload directory:", uploadDir);
-console.log("File path:", filePath);
 
   return {
     url: `/uploads/media/tenant-${tenantId}/${fileName}`,
