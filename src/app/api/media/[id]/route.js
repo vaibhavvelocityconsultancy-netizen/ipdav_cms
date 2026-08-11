b837ca249c0bf194afedc0052c4efc98a8ac668e
Vaibhav Upadhyay <vaibhav.velocityconsultancy@gmail.com>
2026-08-11 15:35:20 +0530

    fix: update media retrieval to use prisma and handle not found response

--- src/app/api/media/[id]/route.js
+++ src/app/api/media/[id]/route.js
@@ -1,19 +1,25 @@
 import fs from "fs/promises";
 import path from "path";
-import {
-  getTenantUploadDir,
-} from "@/src/app/lib/utils/uploadconfig";
-import { getMediaById } from "@/src/app/lib/services/media/media.service";
+import { getTenantUploadDir } from "@/src/app/lib/utils/uploadconfig";
+import { prisma } from "@/src/app/lib/prisma";
 
 export async function GET(req, { params }) {
   const { id } = await params;
 
-  const media = await getMediaById(id);
+  const media = await prisma.media.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!media) {
+    return new Response("Media not found", { status: 404 });
+  }
 
   // Read from the actual upload directory
   const filePath = path.join(
     getTenantUploadDir(media.tenantId),
-    media.publicId
+    media.publicId,
   );
 
   const file = await fs.readFile(filePath);
