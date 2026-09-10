c8217ccb181f3f3d42d5f090a61f12bc23ac6afa
Vaibhav Upadhyay <vaibhav.velocityconsultancy@gmail.com>
2026-09-10 17:31:47 +0530

    commit

--- modules/search/src/app/lib/search/extractText.js
+++
@@ -1,7 +0,0 @@
-import { load } from "cheerio";
-
-export function extractSearchableText(html) {
-  const $ = load(html || "");
-  $("script, style").remove();
-  return $.text().replace(/\s+/g, " ").trim();
-}
